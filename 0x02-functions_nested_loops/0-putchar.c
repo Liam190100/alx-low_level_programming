d66a1e166af689c74bc13b4661475f67ca7e643c
Liam190100 <ngugi1652.com>
2023-03-21 11:59:13 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,8 +4,11 @@
  * main - Entry point
  * Return: Always 0
  */
+
 int main(void)
+
 {
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -16,4 +19,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
+
 }
