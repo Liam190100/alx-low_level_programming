3e35732919fb339bf1db35038c1a42dbe37681a7
Liam190100 <ngugi1652.com>
2023-04-12 08:03:21 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,62 +1,73 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+#include "main.h"
 
 /**
- * check_num - checks if a string contains only digits
- * @str: string to be checked
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * Return: 1 if the string contains only digits, 0 otherwise
+ * Return: the int converted from the string
  */
-int check_num(char *str)
+int _atoi(char *s)
 {
-	unsigned int count;
+	int i, d, n, len, f, digit;
 
-	count = 0;
-	while (count < strlen(str))
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		if (!isdigit(str[count]))
-		{
-			return (0);
-		}
+		if (s[i] == '-')
+			++d;
 
-		count++;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
 	}
-	return (1);
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
 
 /**
- * main - prints the sum of integers passed as command line arguments
+ * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: 0 on success, 1 on error
+ * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
-	int count;
-	int str_to_int;
-	int sum = 0;
+	int result, num1, num2;
 
-	count = 1;
-	while (count < argc)
+	if (argc < 3 || argc > 3)
 	{
-		if (check_num(argv[count]))
-		{
-			str_to_int = atoi(argv[count]);
-			sum += str_to_int;
-		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
-
-		count++;
+		printf("Error\n");
+		return (1);
 	}
 
-	printf("%d\n", sum);
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
 
 	return (0);
 }
