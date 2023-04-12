f99cafd786a57015424dc947ea956e4e542d448c
Liam190100 <ngugi1652.com>
2023-04-12 08:40:18 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,53 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
- */
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
-}
-
-/**
- * main - multiplies two numbers
+ * main - prints the minimum number of coins to
+ * change the amout of money and coins 
  * @argc: number of arguments
  * @argv: array of arguments
  *
@@ -55,20 +12,34 @@ int _atoi(char *s)
  */
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
-	if (argc < 3 || argc > 3)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 * num2;
+	num = atoi(argv[1]);
+	result = 0;
+
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+			result++;
+			num -= coins[j];
+		}
+	}
 
 	printf("%d\n", result);
-
 	return (0);
 }
 
