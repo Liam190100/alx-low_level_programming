9d92d2e98fae2236f69b0d40e64960913bb15cb7
Liam190100 <ngugi1652.com>
2023-04-18 08:02:02 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,35 @@
+#define DOG_H
+
+
+	/**
+	 * struct dog - a dog in the structure
+	 * @name: First name
+	 * @age: Second name
+	 * @owner: Third name
+	 *
+	 * Description: Discribe the structure 
+	 */
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
+
+
+	/**
+	 * dog_t - typedef for struct dog
+	 */
+	typedef struct dog dog_t;
+
+
+	void init_dog(struct dog *d, char *name, float age, char *owner);
+	void print_dog(struct dog *d);
+	dog_t *new_dog(char *name, float age, char *owner);
+	void free_dog(dog_t *d);
+	char *_strcpy(char *dest, char *src);
+	int _strlen(char *s);
+
+
+	#endif
+
