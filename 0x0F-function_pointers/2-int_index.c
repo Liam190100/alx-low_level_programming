0bb70ac55e88c7d8af316abe5048206c1c4d5ff8
Liam190100 <ngugi1652.com>
2023-04-20 04:35:30 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 /**
  * int_index - return index place if comparison = true, else -1
- * @array:initialize array
+ * @array:the array
  * @size: size of array
  * @cmp: pointer to function  of  3 in main
  * Return: 0
@@ -13,7 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
-	while (i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 			return (i);
