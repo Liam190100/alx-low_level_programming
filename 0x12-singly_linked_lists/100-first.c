307173ad3de92cc90447d3ed5a1637549acdc828
Johnnjuguna <jnnjuguna8@gmail.com>
2023-04-28 11:53:11 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_before_main - main function is executed.
+ *
+ * use printf executed the message.
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	"I bore my house upon my back!\n");
+}
+
