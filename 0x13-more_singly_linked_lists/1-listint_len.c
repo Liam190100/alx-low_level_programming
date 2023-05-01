8f22eaac1558e33098805099833aa29d444691aa
Johnnjuguna <jnnjuguna8@gmail.com>
2023-05-01 07:16:08 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,18 +3,17 @@
 /**
  * listint_len - print linked lists
  * @h: linked list that print listint_t to traverse the number
- * Return: nodes
+ * Return: nodes of listint_t list
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t num = 0;
+    size_t count = 0;
 
-	while (h != NULL)
-	{
-		num++;
-		h = h->next;
-	}
+    while (h != NULL)
+    {
+        count++;
+        h = h->next;
+    }
 
-	return (number);
+    return (count);
 }
-
