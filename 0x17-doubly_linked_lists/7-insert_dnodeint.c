7e2f7ca7c00c86c171baacf2930eb09635b35212
Liam190100 <jnnjuguna8@gmail.com>
2023-08-18 06:25:38 -0700

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,44 +9,34 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *temp = *h;
-	unsigned int count = 0;
-
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-	return (NULL);
-
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
+	dlistint_t *tmp = *h, *new;
 
 	if (idx == 0)
 	{
-	new_node->next = *h;
-	if (*h != NULL)
-	(*h)->prev = new_node;
-	*h = new_node;
-	return (new_node);
+		return (add_dnodeint(h, n));
 	}
-
-	while (temp != NULL)
-	
-	if (count == idx - 1)
+	for (; idx != 1; idx--)
 	{
-	new_node->next = temp->next;
+		tmp = tmp->next;
+		if (!tmp)
+		{
+			return (NULL);
+		}
 	}
-	if (temp->next != NULL)
+	if (!tmp->next)
 	{
-		temp->next->prev = new_node;
-		temp->next = new_node;
-		new_node->prev = temp;
-		return (new_node);
-	
-	count++;
-	temp = temp->next;
+		return (add_dnodeint_end(h, n));
 	}
+	new = malloc(sizeof(dlistint_t));
 
-	free(new_node); /* If insertion is not possible */
-	return (NULL);
+	if (!new)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+	return (new);
 }
-
