06c1cbdf5529fe651c4f4f44cb960da378aba427
Liam190100 <jnnjuguna8@gmail.com>
2023-08-21 13:56:19 -0700

    time

---
+++ 0x18-dynamic_libraries/100-operacter.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int addition(int i,int j){
+	return(i + j);
+}
+int substraction(int i,int j){
+	return(i - j);
+}
+int multiplication(int i,int j){
+	return(i * j);
+}
+int division(int i,int j){
+	return(i / j);
+}
+int module(int i,int j){
+	return(i % j);
+}
